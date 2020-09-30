83df8f4b71a4ff120b49eac0f332be1a08644780
nnecec <nnecec@outlook.com>
2020-09-30 17:09:48 +0800

    chore: fix lint

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+  if (process.env.NODE_ENV === 'development') {
+    console.log('boop')
   }
-  return a + b;
-};
+  return a + b
+}
